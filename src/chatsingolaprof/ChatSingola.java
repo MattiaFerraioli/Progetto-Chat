f145e27bb454fca87b358f88ed9cc381d7ca6cb3
MattiaFerraioli <ferra97tia@gmail.com>
2015-10-23 22:13:00 +0200

    Update ChatSingola.java

--- src/chatsingolaprof/ChatSingola.java
+++ src/chatsingolaprof/ChatSingola.java
@@ -1,9 +1,28 @@
 
 package chatsingola;
-import java.io.*;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+
 public class ChatSingola {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+       
+       String nomeFile="D:\\NetBeand Project\\ChatSingola\\src\\chatsingola\\Chat.txt";
+       VicePreside p= new VicePreside(nomeFile);
+       Professore pr=new Professore(nomeFile);
+       
+       System.out.println("Digita per avviare la chat");
+       
+       pr.scrivi();
+       pr.leggi();
+       
+       p.scrivi();
+       p.leggi();
+       
+       
        
     }
     
